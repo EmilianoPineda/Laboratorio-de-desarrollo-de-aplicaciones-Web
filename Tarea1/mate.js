d408b47e0d069edb8b74061456ab35bcd447d955
EmilianoPinedaTec <A01332517@tec.mx>
2022-02-25 15:19:56 -0600

    Se ha terminado la tarea 1

--- Tarea1/mate.js
+++ Tarea1/mate.js
@@ -1,9 +1,21 @@
 
 
-exports.producto = function (a,b){
-    return a*b;
+exports.producto = function (n1,n2){
+    return parseFloat(n1) * parseFloat(n2);
 };
 
-exports.suma = function(a,b){
-    return a+b;
+exports.suma = function(n1,n2){
+    return parseFloat(n1) + parseFloat(n2);
+};
+
+exports.resta = function(n1,n2){
+    return parseFloat(n1) - parseFloat(n2);
+};
+
+exports.division = function(n1,n2){
+    return parseFloat(n1) / parseFloat(n2);
+};
+
+exports.modulo = function(n1,n2){
+    return parseFloat(n1) % parseFloat(n2);
 };
