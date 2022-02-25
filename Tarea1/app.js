f4aef718054ff33eed726f6b83b7d00cbec3ae42
EmilianoPinedaTec <A01332517@tec.mx>
2022-02-25 12:39:49 -0600

    Creacion de carpeta tarea

---
+++ Tarea1/app.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const hostname = '127.0.0.1'
+const port = '3000'
+
+exports.producto = function (a,b){
+    return a*b;
+};
+
+exports.suma = function(a,b){
+    return a+b;
+};
+
+
+
+
+
+
